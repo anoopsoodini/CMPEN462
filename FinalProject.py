371c543f8d81b0076b67da0e95247f05e96cb8de
Chris Erhard <erhard810@gmail.com>
2021-05-03 23:47:55 -0400

    removed usesless helper function

--- FinalProject.py
+++ FinalProject.py
@@ -150,18 +150,6 @@ def encrypt(file, name, fileData):
 		csvWrite.writerow(fileData)
 	
 
-def BinaryToDecimal(binary): 
-         
-    binary1 = binary 
-    decimal, i, n = 0, 0, 0
-    while(binary != 0): 
-        dec = binary % 10
-        decimal = decimal + dec * pow(2, i) 
-        binary = binary//10
-        i += 1
-    return (decimal) 
-
-
 def decrypt(symbols, modulation):
 
 	# get just string of modulation scheme
