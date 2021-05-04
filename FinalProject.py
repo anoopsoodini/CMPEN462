9de05b512f55e3db2fe73a801c57dacda1c524b8
anoopsoodini <71987719+anoopsoodini@users.noreply.github.com>
2021-05-04 14:32:12 -0400

    Final Code

--- FinalProject.py
+++ FinalProject.py
@@ -1,16 +1,18 @@
 # Final Project
+# CMPEN 462
 # James Zukowski, Chris Erhard, Anoop Soodini, Arib Hyder
 
 import os 
 import math
 import csv
 import binascii
-
+from cryptography.fernet import Fernet
+import base64
 
 fields = ['FileName', 'BPSK', 'QPSK', '16QAM']
 
 def encrypt(file, name, fileData):
-
+	base = os.path.splitext(name)[0]
 	string = ""
 	BPSK = []
 	QPSK = []
@@ -18,9 +20,7 @@ def encrypt(file, name, fileData):
 
 	# Iterate through text file and read in file contents into variable "string"
 	while True:
-
 		line = file.readline()
-
 		if not line:
 			break
 		else:
@@ -29,16 +29,36 @@ def encrypt(file, name, fileData):
 	# Convert input into its 8-bit ascii format, store in binaryString
 	binaryString = ''.join(format(ord(i), '08b') for i in string)
 	
-	# Used for testing. Just writing binary data after converion from ASCII to txt file to check correctness
-	encoded = open("encoded.txt", "w")
+	# Writing binary data after converion from ASCII to binary to txt file
+	encoded = open(base+"_toBinary.txt", "w")
 	encoded.write(binaryString)
 
-	#
-	#
-	# TO DO: Encrypt the binary data before modulating
-	#
-	#
+	# Encryption
+	# key generation
+	key = Fernet.generate_key()
+	
+	# string the key in a file
+	with open('filekey.key', 'wb') as filekey:
+		filekey.write(key)
 
+	# opening the key
+	with open('filekey.key', 'rb') as filekey:
+		key = filekey.read()
+
+	# using the generated key
+	fernet = Fernet(key)
+	
+	# encrypting the binary data
+	encrypted = fernet.encrypt(binaryString.encode("utf-8"))
+	
+	# decode encrypted encoded binary data and convert to binary string
+	encrypted = ''.join(format(ord(i), '08b') for i in encrypted.decode('utf-8'))
+	
+	# write encyrpted binary data to file
+	with open(base+'_encryptedBinary_in.txt', 'w') as encrypted_file:
+		encrypted_file.write(encrypted)
+		
+	# Modulate encrypted data
 	while True:
 		print("What modulation scheme would you like to use to encrypt your file?")
 		modulationChoice = int(input("Enter (1) for BPSK, (2) for QPSK, or (3) for 16QAM:\n"))
@@ -51,109 +71,114 @@ def encrypt(file, name, fileData):
 
 	# BPSK Modulation
 	if (modulationChoice == 1):
-		for i in binaryString:
+		for i in encrypted:
 			if(i == '0'):
 				BPSK.append(complex((1/math.sqrt(2)), (1/math.sqrt(2))))
 			elif(i == '1'):
 				BPSK.append(complex((-1/math.sqrt(2)), (-1/math.sqrt(2))))
 		
-		base = os.path.splitext(name)[0]
-		f = open("BPSK"+base+".csv", "w+")
+		f = open(base+"_BPSK.csv", "w+")
 
 		for i in BPSK:
 			f.write(str(complex(i))+",")
 
-		# add modulation scheme for decryption
-		f.write('\n' + "BPSK")
+		# add modulation scheme and key for decryption
+		f.write('\n' + "BPSK" + '\n' + key.decode('utf-8'))
 		f.close()
 
-		fileData["BPSK"] = "BPSK"+base+".csv"
+		fileData["BPSK"] = base+"_BPSK.csv"
+		print("Successfully Encrypted file! Encrypted binary data is stored in " + base +"_encryptedBinary_in.txt.")
+		print("OFDM Symbols, modulation type, and encryption key are stored in " + base + "_BPSK.csv\n")
 
 
 	# QPSK Modulation
 	if (modulationChoice == 2):
-		for i in range(0, len(binaryString), 2):
-			if(binaryString[i:i+2] == '00'):
+		for i in range(0, len(encrypted), 2):
+			if(encrypted[i:i+2] == '00'):
 				QPSK.append(complex((1/math.sqrt(2)), (1/math.sqrt(2))))
-			elif(binaryString[i:i+2] == '01'):
+			elif(encrypted[i:i+2] == '01'):
 				QPSK.append(complex((1/math.sqrt(2)), (-1/math.sqrt(2))))
-			elif(binaryString[i:i+2] == '10'):
+			elif(encrypted[i:i+2] == '10'):
 				QPSK.append(complex((-1/math.sqrt(2)), (1/math.sqrt(2))))
-			elif(binaryString[i:i+2] == '11'):
+			elif(encrypted[i:i+2] == '11'):
 				QPSK.append(complex((-1/math.sqrt(2)), (-1/math.sqrt(2))))
 
-		base = os.path.splitext(name)[0]
-
-		f = open("QPSK"+base+".csv", "w+")
+		f = open(base+"_QPSK.csv", "w+")
 
 		for i in QPSK:
 			f.write(str(complex(i))+",")
 		
-		# add modulation scheme for decryption
-		f.write('\n' + "QPSK")
+		# add modulation scheme and key for decryption
+		f.write('\n' + "QPSK" + '\n' + key.decode('utf-8'))
 		f.close()
 
-		fileData["QPSK"] = "QPSK"+base+".csv"
+		fileData["QPSK"] = base+"_QPSK.csv"
+		print("Successfully Encrypted file! Encrypted binary data is stored in " + base +"_encryptedBinary_in.txt.")
+		print("OFDM Symbols, modulation type, and encryption key are stored in " + base + "_QPSK.csv\n")
 
 	# 16 QAM
 	if (modulationChoice == 3):
-		for i in range(0, len(binaryString), 4):
-			if (binaryString[i:i+4] == '0000'):
+		for i in range(0, len(encrypted), 4):
+			if (encrypted[i:i+4] == '0000'):
 				QAM.append(complex((1/math.sqrt(10)), (1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0001'):
+			if (encrypted[i:i+4] == '0001'):
 				QAM.append(complex((1/math.sqrt(10)), (3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0010'):
+			if (encrypted[i:i+4] == '0010'):
 				QAM.append(complex((3/math.sqrt(10)), (1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0011'):
+			if (encrypted[i:i+4] == '0011'):
 				QAM.append(complex((3/math.sqrt(10)), (3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0100'):
+			if (encrypted[i:i+4] == '0100'):
 				QAM.append(complex((1/math.sqrt(10)), (-1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0101'):
+			if (encrypted[i:i+4] == '0101'):
 				QAM.append(complex((1/math.sqrt(10)), (-3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0110'):
+			if (encrypted[i:i+4] == '0110'):
 				QAM.append(complex((3/math.sqrt(10)), (-1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '0111'):
+			if (encrypted[i:i+4] == '0111'):
 				QAM.append(complex((3/math.sqrt(10)), (-3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1000'):
+			if (encrypted[i:i+4] == '1000'):
 				QAM.append(complex((-1/math.sqrt(10)), (1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1001'):
+			if (encrypted[i:i+4] == '1001'):
 				QAM.append(complex((-1/math.sqrt(10)), (3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1010'):
+			if (encrypted[i:i+4] == '1010'):
 				QAM.append(complex((-3/math.sqrt(10)), (1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1011'):
+			if (encrypted[i:i+4] == '1011'):
 				QAM.append(complex((-3/math.sqrt(10)), (3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1100'):
+			if (encrypted[i:i+4] == '1100'):
 				QAM.append(complex((-1/math.sqrt(10)), (-1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1101'):
+			if (encrypted[i:i+4] == '1101'):
 				QAM.append(complex((-1/math.sqrt(10)), (-3/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1110'):
+			if (encrypted[i:i+4] == '1110'):
 				QAM.append(complex((-3/math.sqrt(10)), (-1/math.sqrt(10))))
-			if (binaryString[i:i+4] == '1111'):
+			if (encrypted[i:i+4] == '1111'):
 				QAM.append(complex((-3/math.sqrt(10)), (-3/math.sqrt(10))))
 
-		base = os.path.splitext(name)[0]
-		f = open("16QAM"+base+".csv", "w+")
+		f = open(base+"_16QAM.csv", "w+")
 
 		for i in QAM:
 			f.write(str(complex(i))+",")
 		
-		# add modulation scheme for decryption
-		f.write('\n' + "QAM")
+		# add modulation scheme and key for decryption
+		f.write('\n' + "QAM" + '\n' + key.decode('utf-8'))
 		f.close()
 
-		fileData["16QAM"] = "16QAM"+base+".csv"
+		fileData["16QAM"] = base+"_16QAM.csv"
+		print("Successfully Encrypted file! Encrypted binary data is stored in " + base +"_encryptedBinary_in.txt.")
+		print("OFDM Symbols, modulation type, and encryption key are stored in " + base + "_16QAM.csv\n")
 
 
 	with open('FileHandle.csv', 'w+') as fh:
 		csvWrite = csv.DictWriter(fh, fieldnames = fields)
 		csvWrite.writeheader()
 		csvWrite.writerow(fileData)
+
+
 	
 
-def decrypt(symbols, modulation):
-
-	# get just string of modulation scheme
+def decrypt(name, symbols, modulation, key):
+	base = os.path.splitext(name)[0]
+	# get just string of modulation scheme and get decryption key
 	modulation = modulation.strip("[']")
+	key = key.strip("[']").encode('utf-8')
 
 	# variable to store output
 	out = ''
@@ -164,7 +189,6 @@ def decrypt(symbols, modulation):
 		for i in symbols:
 			if(complex(i) == complex((1/math.sqrt(2)), (1/math.sqrt(2)))):
 				out = out + '0'
-			# elif(complex(i) == complex((-1/math.sqrt(2)), (-1/math.sqrt(2)))):
 			else:
 				out = out + '1'
 	# QPSK
@@ -215,24 +239,42 @@ def decrypt(symbols, modulation):
 				out = out + '1111'
 
 
-	# Used for testing. Just writing binary data after demodulation to txt file to check correctness
-	outputBinary = open("outputBinary.txt", "w")
+	# Writing encrypted binary data after demodulation to txt file
+	outputBinary = open(base+"_encryptedBinary_out.txt", "w")
 	outputBinary.write(out)
 
-	#
-	#
-	# TO DO: Decrypt the binary data after demodulating
-	#
-	#
-
-
+	# retrieve key
+	fernet = Fernet(key)
+	
+	# convert encrypted binary back to ASCII chars for encoding
 	# separates the binary by every 8 bits for conversion to ASCII
 	byte_array = []
 	for i in range(0, len(out), 8):
 		byte_array.append(out[i : i+8])
+	
+	encryptedASCII = ''
+	# converts every byte to ASCII char for decryption
+	for b in byte_array:
+		try:
+			n = int(b, 2)
+			encryptedASCII = encryptedASCII + str(n.to_bytes((n.bit_length() + 7) // 8, 'big').decode())
+		except:
+			continue
+	
+	# Encoding and then decrypting each byte of the encrypted data
+	decrypted = fernet.decrypt(encryptedASCII.encode('utf-8'))
+
+	# write decyrpted binary data to file
+	with open(base+'_decryptedBinary.txt', 'wb') as decrypted_file:
+		decrypted_file.write(decrypted)
+
+	# separates the decrypted binary by every 8 bits for conversion to ASCII
+	byte_array = []
+	for i in range(0, len(decrypted), 8):
+		byte_array.append(decrypted[i : i+8])
 		
 	outputText = ''
-	# converts every byte to ASCII
+	# converts every byte of decrypted binary data to ASCII text
 	for b in byte_array:
 		try:
 			n = int(b, 2)
@@ -240,11 +282,13 @@ def decrypt(symbols, modulation):
 		except:
 			continue
 	
-	# writes converted ASCII characters to output.txt file
-	outputTextFile = open("outputText.txt", "w")
+	# writes converted ASCII text to base_decryptedText.txt file for final output
+	outputTextFile = open(base+"_decryptedText.txt", "w")
 	outputTextFile.write(outputText)
 
-	print("Successfully Decrypted file! Results are stored in outputText.txt\n")
+	print("Successfully Decrypted file! Results are stored in " + base +"_decryptedText.txt\n")
+
+
 
 
 def main():
@@ -258,14 +302,17 @@ def main():
 	while runChoice:
 
 		while True:
+			# choose what user wants to do 
 			choice = int(input("Would you like to Encrypt (1) or Decrypt (2) a file?:\n"))
 			
+			# check validity of input
 			if ((choice != 1) and (choice != 2)):
 				print("Invalid Input")
 			
 			else:
 				break
 
+			# begin process
 		try:
 			fName = input("Please enter the name of the file you wish to encrypt or decrypt:\n")
 			if(choice == 1):
@@ -278,8 +325,9 @@ def main():
 						# store data (there are only 2 rows)
 						symbols.append((row))
 					
-					# get data into single array and moulation scheme into string variable
+					# get data, moulation scheme, and key for decryption
 					modulation = str(symbols[1])
+					key = symbols[2][0]
 					symbols = symbols[0]
 					for i in range(len(symbols)):
 						# remove parenthesis
@@ -288,22 +336,25 @@ def main():
 					symbols.remove("")
 					
 		except:
+			# error if file not valid
 			print("Unable to locate file")
 
 		finally:
 			fileData['FileName'] = fName
 
+			# store file data
 			with open('FileHandle.csv', 'w') as fh:
 				csvWrite = csv.DictWriter(fh, fieldnames = fields)
 				csvWrite.writeheader()
 				csvWrite.writerow(fileData)
-
+			# encrypt
 			if(choice == 1):
 				encrypt(fOpen, fName, fileData)
-
+			# decrypt
 			if(choice == 2):
-				decrypt(symbols, modulation)
+				decrypt(fName, symbols, modulation, key)
 
+			# see if user wants to continue and end if not
 			userChoice = int(input("Please enter (0) to encrypt/decrypt again. If not, type any other key.\n"))
 		if(userChoice != 0):
 			runChoice = False
@@ -312,4 +363,3 @@ def main():
 
 if(__name__ == '__main__'):
 	main()
-
